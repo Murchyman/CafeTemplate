7e9b158589573f78a226299a34c8e338ddede915
Murchyman <N11099887@qut.edu.au>
2022-04-18 13:44:48 +1000

    admin console

--- components/AdminDash.js
+++ components/AdminDash.js
@@ -1,6 +1,27 @@
-import React from 'react'
-import globalStyles from '../styles/global.js'
+import React from "react";
+import globalStyles from "../styles/global.js";
+import TextField from "@mui/material/TextField";
+import { useEffect } from "react";
+
 const AdminDash = (props) => {
+    useEffect(() => {
+        GetJson();
+    }, []);
+
+    const [TextFieldValue, setTextFieldValue] = React.useState("");
+    const handleChange = (event) => {
+        setTextFieldValue(event.target.TextFieldValue);
+    };
+
+    const GetJson = () => {
+        fetch(
+            // add to env variable
+            process.env.NEXT_PUBLIC_S3URL
+        )
+            .then((res) => res.json())
+            .then((data) => JSON.stringify(data))
+            .then((data) => setTextFieldValue(data));
+    };
 
     return (
         <div>
@@ -9,23 +30,40 @@ const AdminDash = (props) => {
             </style>
             <style jsx>
                 {`
-        .wrapper {
-          width: 100vw;
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-        }
-      `}
+          .wrapper {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          .body {
+            width: 50vw;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+        `}
             </style>
             <div className="wrapper">
-                <h1>Admin</h1>
-                <p>Welcome to the admin page</p>
-                <button onClick={() => props.ToggleAuthentication()}>Logout</button>
+                <div className="body">
+                    <h1>Admin</h1>
+                    <p>Welcome to the admin page</p>
+
+                    <TextField
+                        id="outlined-multiline-static"
+                        multiline
+                        fullWidth
+                        min-rows={4}
+                        value={TextFieldValue}
+                        onChange={handleChange}
+                    />
+                    <button onClick={() => props.ToggleAuthentication()}>Logout</button>
+                </div>
             </div>
         </div>
     );
-}
+};
 
-export default AdminDash
+export default AdminDash;
