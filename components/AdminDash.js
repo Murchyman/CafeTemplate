6406dc886c5a7ab2bd2d3e201874bb0ad45842f0
Murchyman <N11099887@qut.edu.au>
2022-05-01 12:03:29 +1000

    modal working

--- components/AdminDash.js
+++ components/AdminDash.js
@@ -1,55 +1,61 @@
 import React from "react";
 import Head from "next/head";
 import useFetch from "../hooks/useFetch.js";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import globalStyles from "../styles/global.js";
 import TextField from "@mui/material/TextField";
 import { useEffect } from "react";
 
 const AdminDash = (props) => {
-    const { data, loading } = useFetch(
-        process.env.NEXT_PUBLIC_S3URL
-    );
+  const [TextFieldValue, setTextFieldValue] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const [modalTitle, setModalTitle] = React.useState("");
+  const [modalText, setModalText] = React.useState("");
+  const { data, loading } = useFetch(process.env.NEXT_PUBLIC_S3URL);
 
-
-    useEffect(() => {
-        if (data) {
-            setTextFieldValue(JSON.stringify(data, null, 2));
-        }
-    }, [data]);
-
-
-    function pushData() {
-        const params = {
-            json: JSON.stringify(JSON.parse(TextFieldValue)),
-            file: `${process.env.NEXT_PUBLIC_Name}/Data.json`,
-        };
-        const options = {
-            method: 'POST',
-            body: JSON.stringify(params)
-        };
-
-        fetch('https://nkc4n1ec95.execute-api.ap-southeast-2.amazonaws.com/default/MudjimbaUploadToS3', options)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
+  useEffect(() => {
+    if (data) {
+      //when data is retrieved, set the textfield value
+      setTextFieldValue(JSON.stringify(data, null, 2));
     }
+  }, [data]);
 
-    const [TextFieldValue, setTextFieldValue] = React.useState("");
+  function pushData() {
+    const params = {
+      //json parse normalises the string that was disfigured by html then we can re-stringify it
+      json: JSON.stringify(JSON.parse(TextFieldValue)),
+      file: `${process.env.NEXT_PUBLIC_Name}/Data.json`,
+    };
+    const options = {
+      method: "POST",
+      body: JSON.stringify(params),
+    };
 
+    fetch(
+      "https://nkc4n1ec95.execute-api.ap-southeast-2.amazonaws.com/default/MudjimbaUploadToS3",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        alert(data);
+      });
+  }
 
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
 
-    return (
-        <div>
-            <Head>
-                <title>Dashboard</title>
-            </Head>
-
-            <style jsx global>
-                {globalStyles}
-            </style>
-            <style jsx>
-                {`
+      <style jsx global>
+        {globalStyles}
+      </style>
+      <style jsx>
+        {`
           .wrapper {
             width: 100vw;
             height: 100vh;
@@ -58,33 +64,126 @@ const AdminDash = (props) => {
             align-items: center;
           }
           .body {
-            width: 50vw;
+            width: 70vw;
             height: 100vh;
             display: flex;
             flex-direction: column;
             align-items: center;
           }
+          .Buttons {
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            margin-top: 2em;
+            width: 100%;
+          }
+          .Button {
+            margin-right: 1em;
+          }
+          .Help {
+            position: absolute;
+            top: 1em;
+            right: 1em;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            width: 13em;
+          }
         `}
-            </style>
-            <div className="wrapper">
-                <div className="body">
-                    <h1>Admin</h1>
-                    <p>Welcome to the admin page</p>
-                    <TextField
-                        style={{ backgroundColor: "white" }}
-                        id="outlined-multiline-static"
-                        multiline
-                        fullWidth
-                        min-rows={4}
-                        value={TextFieldValue}
-                        onChange={(event) => setTextFieldValue(event.target.value)}
-                    />
-                    <button onClick={pushData}>Push Data</button>
-                    <button onClick={() => props.ToggleAuthentication()}>Logout</button>
-                </div>
-            </div>
+      </style>
+
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+            color: "text.primary",
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {modalTitle}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {modalText}
+          </Typography>
+        </Box>
+      </Modal>
+
+      <div className="wrapper">
+        <div className="body">
+          <h1>Admin</h1>
+          <p>Please call Mitch if you are confused 0490117140</p>
+          <div className="Help">
+            <Button
+              variant="contained"
+              onClick={() => {
+                setOpen(true);
+                setModalTitle("Hard Reset");
+                setModalText(
+                  "This will reset the menu to a known working version in the backend. This is a destructive action and cannot be undone."
+                );
+              }}
+            >
+              hard reset
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setOpen(true);
+                setModalTitle("Help");
+                setModalText(
+                  "This is the admin dashboard. Here you can upload data to the server. This is a JSON file. Please make sure that the data is in the correct format. If you are unsure about the format, please contact Mitch."
+                );
+              }}
+            >
+              Help
+            </Button>
+          </div>
+          <TextField
+            style={{ backgroundColor: "white" }}
+            id="outlined-multiline-static"
+            multiline
+            fullWidth
+            maxRows={25}
+            value={TextFieldValue}
+            onChange={(event) => setTextFieldValue(event.target.value)}
+          />
+          <div className="Buttons">
+            <Button
+              style={{ marginRight: "1em" }}
+              size="large"
+              onClick={() => setTextFieldValue(JSON.stringify(data, null, 2))}
+              variant="contained"
+            >
+              Reset
+            </Button>
+            <Button
+              size="large"
+              onClick={pushData}
+              variant="contained"
+              endIcon={<ArrowForwardIcon />}
+            >
+              Submit
+            </Button>
+          </div>
+
+          {/* <button onClick={() => props.ToggleAuthentication()}>Logout</button> */}
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default AdminDash;
